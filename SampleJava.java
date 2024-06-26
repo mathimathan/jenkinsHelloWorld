510724475f60b161c1f26ea9aa39cfecc6c61131
Mathanmathi M <120257387+mathimathan@users.noreply.github.com>
2024-06-26 17:40:50 +0530

    Update SampleJava.java

--- SampleJava.java
+++ SampleJava.java
@@ -2,5 +2,6 @@ public class SampleJava
 {
    public static void main(String[] arg){
     System.out.println("************ This is my first Java Program ******");
+	   System.out.println("************ New Change has been created ******");
 	}
 	}
